a4ed83315b6f53f3ce13d56ac6079a295a11d610
Nastya <kechnastya@gmail.com>
2023-11-17 17:14:39 +0300

    add replacer and spacesCount

--- src/compareStylish.js
+++ src/compareStylish.js
@@ -1,15 +1,15 @@
-const compareStylish = (comparisonTree) => {
+const compareStylish = (comparisonTree, replacer = ' ', spacesCount = 2) => {
   const result = comparisonTree.map((node) => {
     if (node.state === 'added') {
-      return `  + ${node.key}: ${node.value}`;
+      return `${replacer.repeat(spacesCount)}+ ${node.key}: ${node.value}`;
     }
     if (node.state === 'deleted') {
-      return `  - ${node.key}: ${node.value}`;
+      return `${replacer.repeat(spacesCount)}- ${node.key}: ${node.value}`;
     }
     if (node.state === 'changed') {
-      return `  - ${node.key}: ${node.value1}\n  + ${node.key}: ${node.value2}`;
+      return `${replacer.repeat(spacesCount)}- ${node.key}: ${node.value1}\n${replacer.repeat(spacesCount)}+ ${node.key}: ${node.value2}`;
     }
-    return `    ${node.key}: ${node.value}`;
+    return `${replacer.repeat(spacesCount * 2)}${node.key}: ${node.value}`;
   });
   return ['{', ...result, '}'].join('\n');
 };
